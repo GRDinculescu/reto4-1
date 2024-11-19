97c5ee8807654e7ab03ee80f7fe54221ec8529f9
JorgeRuedaP <jorgeruedaplo2004@gmail.com>
2024-11-19 11:45:04 +0100

    Modificacion

--- Reto4/src/Ejercicios/reto4.java
+++ Reto4/src/Ejercicios/reto4.java
@@ -1,10 +1,21 @@
 package Ejercicios;
 
+import java.util.Scanner;
+
 public class reto4 {
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		int num=0;
 		do {
-			
-		} while (num%2==0);
+			try {
+				num=Funciones.dimeEntero("Escribe un numero", sc);
+				sc.nextInt();
+			} catch (Exception e) {
+				System.out.println("Escribe un numero valido");
+			}
+		} while (num%2==0 || num%3==0 || num%5==0);
+		Fichero1.divisores(num);
+		System.out.println(factorial(sum));
+		sc.close();
 	}
 }
