7e3c47cb59fd29bde04f6c6dee995c736c0c5473
mariopellicer <marioopelli23@gmail.com>
2024-11-19 11:46:09 +0100

    commit de fichero 1 que hace los divisores del numero y la suma total

--- Reto4/src/Ejercicios/Fichero1.java
+++ Reto4/src/Ejercicios/Fichero1.java
@@ -2,4 +2,18 @@ package Ejercicios;
 
 public class Fichero1 {
 
+	//sacar los divisores por pantalla y devuelve su suma total 
+	public static int divisoresSuma(int num) {
+		int p=0;
+		int pm=0;
+		for (int i = 0; i > num; i++) {
+			if (i%num==0) {
+				System.out.println(i);
+				p++;
+			}
+			pm +=p;
+			System.out.println(pm);
+		}
+	}
+
 }
