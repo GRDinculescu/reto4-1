4c566d81cf909e45e796b56f440add7ad6518561
pilar <piruletaabad2002@gmail.com>
2024-11-19 11:41:04 +0100

    fichero2 hecho

--- Reto4/src/Ejercicios/Fichero2.java
+++ Reto4/src/Ejercicios/Fichero2.java
@@ -2,9 +2,18 @@ package Ejercicios;
 
 public class Fichero2 {
 	
-	/*Esta funcion debe sacar el número factorial menor más cercano a la suma de todos los números*/
-	public static void factorial (int suma) {
-		
+	/*Esta funcion debe sacar el número cuyo factorial es el menor más cercano a la suma de todos los números anteriores*/
+	public static int factorial (int suma) {
+		int kk = 1, bueno;
+		for (int i = 0; i < Math.pow(suma, suma); i++) {
+			for (int j = 0; j < i; j++) {
+				kk *= i;
+			}
+			if (kk > suma) {
+				bueno = kk;
+			}
+		}
+		return bueno;
 	}
 
 }
